d70f13d1db76f0b3b89d0b993eff59df52e17d24
Alaa5090 <alaaabdraboo297@gmail.com>
2021-10-14 16:22:53 +0200

    adding gui pkg

---
+++ gui/target/rls/debug/deps/gui-3208c91e909218a4.d
@@ -0,0 +1,5 @@
+c:\Users\Mostafa\Desktop\rust_design_patterns\oop_design_pattern\gui\target\rls\debug\deps\gui-3208c91e909218a4.rmeta: src\lib.rs
+
+c:\Users\Mostafa\Desktop\rust_design_patterns\oop_design_pattern\gui\target\rls\debug\deps\gui-3208c91e909218a4.d: src\lib.rs
+
+src\lib.rs:
---
+++ gui/target/rls/debug/deps/gui-358cfb53464d8b4e.d
@@ -0,0 +1,5 @@
+c:\Users\Mostafa\Desktop\rust_design_patterns\oop_design_pattern\gui\target\rls\debug\deps\gui-358cfb53464d8b4e.rmeta: src\lib.rs
+
+c:\Users\Mostafa\Desktop\rust_design_patterns\oop_design_pattern\gui\target\rls\debug\deps\gui-358cfb53464d8b4e.d: src\lib.rs
+
+src\lib.rs:
