ded541ec8b6f6d9e40fe9f724436f6cb75c4e5e6
Alaa5090 <alaaabdraboo297@gmail.com>
2021-10-23 00:17:44 +0200

    trait objects demo

--- gui/src/lib.rs
+++ gui/src/lib.rs
@@ -1,3 +1,42 @@
+pub trait Draw {
+     fn draw(&self);
+
+}
+ pub struct Screen{
+pub components:Vec<Box<dyn Draw>>,
+
+}
+impl Screen {
+    pub fn run(&self)  {
+       for component in self.components.iter() {
+           component.draw();
+       }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #[cfg(test)]
 mod tests {
     #[test]
